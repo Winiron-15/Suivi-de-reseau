88a7560f14e37c986c84eb925c7952f249b6b180
Winiron-15 <geraudgauzins15@gmail.com>
2025-04-01 15:19:36 +0200

    refactor: restructure project into src/ layout vfinal

--- src/arguments.py
+++ src/arguments.py
@@ -3,7 +3,6 @@ import argparse
 def parse_arguments():
     """Parses command-line arguments."""
     parser = argparse.ArgumentParser(description="Network Scanner Tool")
-    parser.add_argument("--input", required=True, help="Input CSV file with machine names and IPs")
-    parser.add_argument("--output", default="results.csv", help="Output file for scan results")
+    parser.add_argument("--file", required=True, help="Input CSV file with machine names and IPs")
     parser.add_argument("--threads", type=int, default=10, help="Number of threads for scanning")
     return parser.parse_args()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from src.arguments import parse_arguments
-from src.scanner import scan_ips
-from src.utils import read_from_csv, save_to_csv
+from arguments import parse_arguments
+from scanner import scan_ips
+from utils import read_from_csv, save_to_csv
 
 def main():
     args = parse_arguments()
 
     # Lire les IP et les noms des machines depuis le fichier CSV
-    machines = read_from_csv(args.input)
+    machines = read_from_csv(args.file)
 
     print("Starting network scan...")
     results = scan_ips(machines, args.threads)
@@ -14,11 +14,11 @@ def main():
     for result in results:
         print(f"Machine: {result[0]} - IP: {result[1]} - Status: {result[2]} - Ping: {result[3]} ms")
 
-    save_to_csv(results, args.output)
-    print(f"Results saved to {args.output}")
+    save_to_csv(results, "data/results.csv")
+    print(f"Results saved to data/results.csv")
 
 
 if __name__ == "__main__":
     main()
 
-#Exécuter le script avec "python src/main.py --input data/machines.csv --output data/results.csv"
+#Exécuter le script avec "python src/main.py --file data/machines.csv"
